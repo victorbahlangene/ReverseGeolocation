4efb49d25c7189ab5ef2d0d7c735838e6b791170
Victor Bahlangene <victorBahlangene96@gmail.com>
2019-01-28 00:13:06 +0300

    Updates

---
+++ precache-manifest.e169c8e8f0db76089f4667e1491f7dd3.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "7183ceccd2621dfd8853",
+    "url": "/ReverseGeolocation/static/css/main.267c3d93.chunk.css"
+  },
+  {
+    "revision": "7183ceccd2621dfd8853",
+    "url": "/ReverseGeolocation/static/js/main.7183cecc.chunk.js"
+  },
+  {
+    "revision": "be37b0be147f030c28f7",
+    "url": "/ReverseGeolocation/static/css/1.728e0001.chunk.css"
+  },
+  {
+    "revision": "be37b0be147f030c28f7",
+    "url": "/ReverseGeolocation/static/js/1.be37b0be.chunk.js"
+  },
+  {
+    "revision": "8cb971f3a22c3af9c425",
+    "url": "/ReverseGeolocation/static/js/runtime~main.8cb971f3.js"
+  },
+  {
+    "revision": "50b3923f8239afb41d996c664bf76724",
+    "url": "/ReverseGeolocation/static/media/ciuen.50b3923f.png"
+  },
+  {
+    "revision": "96f6524d99250cdb96f94ad09ef7fff8",
+    "url": "/ReverseGeolocation/index.html"
+  }
+];
